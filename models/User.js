ac78a758ce5938ed2376e549145e03f1ea214ea3
Joshua Weisbrod-Torres <jwtorres@rocketmail.com>
2020-09-19 18:14:45 -0500

    models are built

---
+++ models/User.js
@@ -0,0 +1,53 @@
+const { Schema, model } = require('mongoose');
+
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    lowercase: true,
+    validate: {
+      validator: function (email) {
+        return /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]/.test(email)
+      }
+    } 
+  },
+  thoughts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Thought'
+    }
+  ],
+  friends: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    }
+  ]
+},
+{
+  toJSON: {
+    virtuals: true,
+    getters: true
+  }
+}
+)
+
+UserSchema.virtual('friendCount').get(function() {
+  return this.friends.length;
+});
+
+// UserSchema.path('email').validate(function(email) {
+//   return
+// })
+
+const User = model('User', UserSchema)
+
+module.exports = User;
